f759bc1a9f08133e52eebf3a08639676f6c60bbd
srinath748 <msrinath748@gmail.com>
2025-09-11 12:11:00 +0530

    light mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 // tailwind.config.js
+
 module.exports = {
   content: ["./index.html", "./script.js"], 
   theme: {
     extend: {},
   },
   plugins: [],
+  darkMode: false, // disable dark mode entirely
+  theme: {
+    extend: {
+      colors: {
+        background: "#111827", // gray-900
+        card: "#1f2937",      // gray-800
+        text: "#d1d5db"       // gray-300
+      }
+    }
+  }
 }
